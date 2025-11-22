0735ac4e34bb09850c265a29839cd24c6e863cc9
Iraklibedoshvili <iraklibedoshvili9@gmail.com>
2025-11-23 00:33:32 +0400

    commit

---
+++ day 74/Homework/script.js
@@ -0,0 +1,15 @@
+const div=document.getElementsByTagName("div")
+console.log("div",alldivs)
+
+const products=document.getElementsByClassName("product")
+console.log("all product",products)
+
+const SecondProduct= products[1]
+console.log("Product:",SecondProduct.previousElementSibling)
+
+console.log("product",SecondProduct.nextElementSibling)
+
+const FirstProduct=document.getElementById("first product")
+console.log("product",FirstProduct.firstChild)
+
+console.log("product:",FirstProduct.lastChild)
