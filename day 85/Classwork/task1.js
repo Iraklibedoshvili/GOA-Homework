c7fecb248e71aaef5078a788c9d3baf8d4c81f9f
Iraklibedoshvili <iraklibedoshvili9@gmail.com>
2025-12-28 01:04:02 +0400

    commit

---
+++ day 85/Classwork/task1.js
@@ -0,0 +1,14 @@
+const jsonData = 
+[
+    {
+        name:irakli,
+        balance: 2300
+    },
+    {
+        name:iraKLiI,
+        balance:3400
+    }
+]
+
+const accounts = JSON.parse(jsonData);
+console.log(accounts)
